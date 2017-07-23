9ffb6c780e1f5458a5cfcb08983b306724fede9d
Tyler Martin <tyler.biron.martin@gmail.com>
2017-07-22 21:05:13 -0400

    typyMagics is a go
    Created a ipython magic that both runs and cell and then writes it to
    disk. This is helpful because it allows a Jupyter notebook to be a
    development platform for a codebase that will not nessecarily be
    run from a notebook.

--- nb/py/typyMagics.py
+++ nb/py/typyMagics.py
@@ -1,22 +1,31 @@
 from __future__ import print_function
 import io
 import os
+
 from IPython.core import magic_arguments
-from IPython.core.magic import  (
-    Magics, compress_dhist, magics_class, line_magic, cell_magic, line_cell_magic
-)
+from IPython.core.magic import  Magics,magics_class, line_magic, cell_magic
 
 @magics_class
 class typyMagics(Magics):
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument(
+        '-a', '--append', action='store_true', default=False,
+        help='Append contents of the cell to an existing file. '
+             'The file will be created if it does not exist.'
+    )
+    @magic_arguments.argument(
+        'filename', type=str,
+        help='file to write'
+    )
     @cell_magic
-    def write_and_run(self, line, cell):
+    def run_and_write(self, line, cell):
         """Write the contents of the cell to a file.
         
         The file will be overwritten unless the -a (--append) flag is specified.
         """
         self.shell.run_cell(cell)
 
-        args = magic_arguments.parse_argstring(self.writefile, line)
+        args = magic_arguments.parse_argstring(self.run_and_write, line)
         filename = os.path.expanduser(args.filename)
 
         if os.path.exists(filename):
